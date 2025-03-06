f7d0199fd7a94e9956383b996680a0d9706e3b38
Larong <barakinlt@gmail.com>
2025-03-06 22:47:03 +0300

    a little change

--- src/main/java/com/stealer/game/Game.java
+++ src/main/java/com/stealer/game/Game.java
@@ -1,10 +1,5 @@
 package com.stealer.game;
 
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_DOWN;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_LEFT;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_RIGHT;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_UP;
-
 import java.util.Vector;
 
 import com.stealer.core.KeyboardInput;
